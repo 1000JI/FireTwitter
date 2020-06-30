fab796d16bcc29af3adc00cacfdc6726b1ea98f0
1000JI <chjh1992@gmail.com>
2020-06-30 21:30:33 +0900

    feat: Getting Started With Login UI

---
+++ FireTwitter/FireTwitter/Controller/Authentication/LoginController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginController.swift
+//  FireTwitter
+//
+//  Created by 천지운 on 2020/06/30.
+//  Copyright © 2020 jwcheon. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = #imageLiteral(resourceName: "TwitterLogo")
+        return iv
+    }()
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isHidden = true
+        
+        view.addSubview(logoImageView)
+        logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
+        logoImageView.setDimensions(width: 150, height: 150)
+        logoImageView.layer.cornerRadius = 150 / 2
+    }
+    
+}
---
+++ FireTwitter/FireTwitter/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationController.swift
+//  FireTwitter
+//
+//  Created by 천지운 on 2020/06/30.
+//  Copyright © 2020 jwcheon. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+    
+}
