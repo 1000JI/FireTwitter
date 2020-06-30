63bfb92626800173975a59d82eae13df1dae4c81
1000JI <chjh1992@gmail.com>
2020-06-30 11:25:45 +0900

    feat: Configure Project & Setup Root ViewController

---
+++ FireTwitter/FireTwitter/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  FireTwitter
+//
+//  Created by 천지운 on 2020/06/30.
+//  Copyright © 2020 jwcheon. All rights reserved.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+    
+}
