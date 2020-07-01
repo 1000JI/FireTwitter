28484129e52c7c988978031ed307a6edd35673f1
1000JI <chjh1992@gmail.com>
2020-07-01 17:23:16 +0900

    feat: Upload Tweet Controller Setup

---
+++ FireTwitter/FireTwitter/Controller/UploadTweetController.swift
@@ -0,0 +1,61 @@
+//
+//  UploadTweetController.swift
+//  FireTwitter
+//
+//  Created by 천지운 on 2020/07/01.
+//  Copyright © 2020 jwcheon. All rights reserved.
+//
+
+import UIKit
+
+class UploadTweetController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private lazy var actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .twitterBlue
+        button.setTitle("Tweet", for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        
+        button.frame = CGRect(x: 0, y: 0, width: 64, height: 32)
+        button.layer.cornerRadius = 32 / 2
+        
+        button.addTarget(self, action: #selector(handleUploadTweet), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleCancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func handleUploadTweet() {
+        
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationController?.navigationBar.barTintColor = .white
+        navigationController?.navigationBar.isTranslucent = false // false로 하지 않으면 기본적으로 반투명 색상
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: actionButton)
+    }
+    
+    // MARK: - API
+    
+}
