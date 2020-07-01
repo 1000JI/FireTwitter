0b686cfb4d60f80e73f4ba6606a020827b745dbd
1000JI <chjh1992@gmail.com>
2020-07-02 00:27:41 +0900

    feat: Fetch Tweets From Firebase & Create Tweet Object

---
+++ FireTwitter/FireTwitter/Model/Tweet.swift
@@ -0,0 +1,31 @@
+//
+//  Tweet.swift
+//  FireTwitter
+//
+//  Created by 천지운 on 2020/07/02.
+//  Copyright © 2020 jwcheon. All rights reserved.
+//
+
+import Foundation
+
+struct Tweet {
+    let caption: String
+    let tweetID: String
+    let uid: String
+    let likes: Int
+    var timestamp: Date!
+    let retweetCount: Int
+    
+    init(tweetID: String, dictionary: [String : Any]) {
+        self.tweetID = tweetID
+        
+        self.caption = dictionary["caption"] as? String ?? ""
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.likes = dictionary["likes"] as? Int ?? 0
+        self.retweetCount = dictionary["retweets"] as? Int ?? 0
+        
+        if let timestamp = dictionary["timestamp"] as? Double {
+            self.timestamp = Date(timeIntervalSince1970: timestamp)
+        }
+    }
+}
