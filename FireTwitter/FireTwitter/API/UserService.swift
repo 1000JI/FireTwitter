737bf880b6b220091e3167d2838900a6a9c2a689
1000JI <chjh1992@gmail.com>
2020-07-01 14:50:07 +0900

    feat: Fetch User Data From Firebase

---
+++ FireTwitter/FireTwitter/API/UserService.swift
@@ -0,0 +1,23 @@
+//
+//  UserService.swift
+//  FireTwitter
+//
+//  Created by 천지운 on 2020/07/01.
+//  Copyright © 2020 jwcheon. All rights reserved.
+//
+
+import Firebase
+
+struct UserService {
+    static let shared = UserService()
+    private init() { }
+    
+    func fetchUser() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshot in
+            guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
+            
+            guard let username = dictionary["username"] as? String else { return }
+        }
+    }
+}
