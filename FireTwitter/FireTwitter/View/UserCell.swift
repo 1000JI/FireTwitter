ab37ae2d779be14bc20f5d6c60feeb4bba528881
1000JI <chjh1992@gmail.com>
2020-07-05 20:47:25 +0900

    feat: Search Page UI

---
+++ FireTwitter/FireTwitter/View/UserCell.swift
@@ -0,0 +1,63 @@
+//
+//  UserCell.swift
+//  FireTwitter
+//
+//  Created by 천지운 on 2020/07/05.
+//  Copyright © 2020 jwcheon. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 40, height: 40)
+        iv.layer.cornerRadius = 40 / 2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 14)
+        label.text = "Username"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.text = "Fullname"
+        return label
+    }()
+    
+    // MARK: - LifeCycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        profileImageView.centerY(inView: self, leadingAnchor: leadingAnchor, paddingLeading: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        stack.axis = .vertical
+        stack.spacing = 2
+        
+        addSubview(stack)
+        stack.centerY(inView: profileImageView, leadingAnchor: profileImageView.trailingAnchor,
+                      paddingLeading: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    
+    
+}
