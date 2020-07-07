6c03d31fe32ac8bf2b458f3a32c3901c0413bd50
1000JI <chjh1992@gmail.com>
2020-07-07 21:16:10 +0900

    feat: Action Sheet Cell & Cancel Button

---
+++ FireTwitter/FireTwitter/View/ActionSheetCell.swift
@@ -0,0 +1,52 @@
+//
+//  ActionSheetCell.swift
+//  FireTwitter
+//
+//  Created by 천지운 on 2020/07/07.
+//  Copyright © 2020 jwcheon. All rights reserved.
+//
+
+import UIKit
+
+class ActionSheetCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private let optionImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = #imageLiteral(resourceName: "twitter_logo_blue")
+        return iv
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18)
+        label.text = "Text Option"
+        return label
+    }()
+    
+    // MARK: - LifeCycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(optionImageView)
+        optionImageView.centerY(inView: self)
+        optionImageView.anchor(leading: leadingAnchor, paddingLeading: 8)
+        optionImageView.setDimensions(width: 36, height: 36)
+        
+        addSubview(titleLabel)
+        titleLabel.centerY(inView: self)
+        titleLabel.anchor(leading: optionImageView.trailingAnchor, paddingLeading: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    
+    
+}
