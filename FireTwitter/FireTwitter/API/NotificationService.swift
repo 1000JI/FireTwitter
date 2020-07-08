7d663a84a8e6c4c65b61e4ec9f2afe8b7dc7a2a1
1000JI <chjh1992@gmail.com>
2020-07-08 13:18:00 +0900

    feat: Upload Like Notification

---
+++ FireTwitter/FireTwitter/API/NotificationService.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationService.swift
+//  FireTwitter
+//
+//  Created by 천지운 on 2020/07/08.
+//  Copyright © 2020 jwcheon. All rights reserved.
+//
+
+import Firebase
+
+struct NotificationService {
+    static let shared = NotificationService()
+    private init() { }
+    
+    func uploadNotification(type: NotificationType, tweet: Tweet? = nil) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        var values: [String: Any] = ["timestamp": Int(NSDate().timeIntervalSince1970),
+                                     "uid": uid,
+                                     "type": type.rawValue]
+        
+        if let tweet = tweet {
+            values["tweetID"] = tweet.tweetID
+            REF_NOTIFICATIONS.child(tweet.user.uid).childByAutoId().updateChildValues(values)
+        } else {
+            
+        }
+    }
+}
