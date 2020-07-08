916997e4a76214ce650810ebcc0219563bc2a6ba
1000JI <chjh1992@gmail.com>
2020-07-08 18:18:13 +0900

    feat: Adding Info To Replies In Firebase & Data Model

--- FireTwitter/FireTwitter/Controller/ProfileController.swift
+++ FireTwitter/FireTwitter/Controller/ProfileController.swift
@@ -79,6 +79,10 @@ class ProfileController: UICollectionViewController {
     func fetchReplies() {
         TweetService.shared.fetchReplies(forUser: user) { tweets in
             self.replies = tweets
+            
+            self.replies.forEach {
+                print($0.replyingTo)
+            }
         }
     }
     
